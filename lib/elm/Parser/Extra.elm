74f7a648922316857b8f1afcdbbf10d59faa6beb
Casey Webb <notcaseywebb@gmail.com>
2023-12-07 06:22:18 -0600

    Parser.Extra.list

--- lib/elm/Parser/Extra.elm
+++ lib/elm/Parser/Extra.elm
@@ -6,3 +6,19 @@ import Parser exposing (..)
 const : String -> a -> Parser a
 const str ctor =
     symbol str |> map (always ctor)
+
+
+list : Parser a -> Parser (List a)
+list parser =
+    loop []
+        (\acc ->
+            spaces
+                |> andThen
+                    (always
+                        (oneOf
+                            [ parser |> map (\item -> Loop (item :: acc))
+                            , succeed (Done (List.reverse acc))
+                            ]
+                        )
+                    )
+        )
